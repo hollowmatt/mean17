c858b0e92527b4e5f6df5e71b3ea914ba31c10ee
Matt Holloway <hollowmatt@gmail.com>
2024-09-02 15:00:31 -0400

    frame out the mongo connection and schema

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,23 @@ var corsOpts = {
 };
 
 app.use(cors(corsOpts));
-
 //parse requests of type 'application/json'
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch(err => {
+    console.log("cannot connect to db...", err);
+    process.exit();
+  });
 
 //simple route
 app.get("/", (req, res) => {
