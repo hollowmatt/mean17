188c73201e5d5bbe47f1c66775d26745019776d4
Matt Holloway <hollowmatt@gmail.com>
2024-09-03 20:09:30 -0400

    wire in details

--- frontend/src/app/components/tutorial-details/tutorial-details.component.ts
+++ frontend/src/app/components/tutorial-details/tutorial-details.component.ts
@@ -1,14 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Tutorial } from '../../models/tutorial.model';
 import { TutorialService } from '../../services/tutorial.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Tutorial } from '../../models/tutorial.model';
 
 @Component({
   selector: 'app-tutorial-details',
   templateUrl: './tutorial-details.component.html',
-  styleUrl: './tutorial-details.component.css'
+  styleUrls: ['./tutorial-details.component.css'],
 })
-export class TutorialDetailsComponent implements OnInit {
+export class TutorialDetailsComponent {
   @Input() viewMode = false;
 
   @Input() currentTutorial: Tutorial = {
@@ -22,24 +22,24 @@ export class TutorialDetailsComponent implements OnInit {
   constructor(
     private tutorialService: TutorialService,
     private route: ActivatedRoute,
-    private router: Router) {}
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-      if (!this.viewMode) {
-        this.message = '';
-        this.getTutorial(this.route.snapshot.params["id"]);
-      }
+    if (!this.viewMode) {
+      this.message = '';
+      this.getTutorial(this.route.snapshot.params['id']);
+    }
   }
 
   getTutorial(id: string): void {
-    this.tutorialService.get(id)
-      .subscribe({
-        next: (data) => {
-          this.currentTutorial = data;
-          console.log(data);
-        },
-        error: (e) => console.error(e)
-      });
+    this.tutorialService.get(id).subscribe({
+      next: (data) => {
+        this.currentTutorial = data;
+        console.log(data);
+      },
+      error: (e) => console.error(e)
+    });
   }
 
   updatePublished(status: boolean): void {
@@ -48,40 +48,44 @@ export class TutorialDetailsComponent implements OnInit {
       description: this.currentTutorial.description,
       published: status
     };
-    
+
     this.message = '';
-    this.tutorialService.update(this.currentTutorial.id, data)
-      .subscribe({
-        next: (res) => {
-          console.log(res);
-          this.currentTutorial.published = status;
-          this.message = res.message ? res.message : "Status updated successfully";
-        },
-        error: (e) => console.error(e)
-      });
+
+    this.tutorialService.update(this.currentTutorial.id, data).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.currentTutorial.published = status;
+        this.message = res.message
+          ? res.message
+          : 'The status was updated successfully!';
+      },
+      error: (e) => console.error(e)
+    });
   }
 
   updateTutorial(): void {
     this.message = '';
 
-    this.tutorialService.update(this.currentTutorial.id, this.currentTutorial)
+    this.tutorialService
+      .update(this.currentTutorial.id, this.currentTutorial)
       .subscribe({
         next: (res) => {
           console.log(res);
-          this.message = res.message ? res.message : 'This tutorial was updated successfully!';
+          this.message = res.message
+            ? res.message
+            : 'This tutorial was updated successfully!';
         },
         error: (e) => console.error(e)
       });
   }
 
   deleteTutorial(): void {
-    this.tutorialService.delete(this.currentTutorial.id)
-      .subscribe({
-        next: (res) => {
-          console.log(res);
-          this.router.navigate(['/tutorials']);
-        },
-        error: (e) => console.error(e)
-      });
+    this.tutorialService.delete(this.currentTutorial.id).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.router.navigate(['/tutorials']);
+      },
+      error: (e) => console.error(e)
+    });
   }
 }
